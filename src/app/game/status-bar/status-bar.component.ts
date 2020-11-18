c79b70cafcecfe0d6a156b35ccf75d8f39dc2a0e
Matthias <maciej.plis.secure@gmail.com>
2020-11-18 02:42:03 +0100

    fix (Game View): TT-34 improve status phrase

--- src/app/game/status-bar/status-bar.component.ts
+++ src/app/game/status-bar/status-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { AuthenticationService } from 'src/app/auth/authentication.service';
 import { GameState, Player } from '../store/game-data.reducer';
 
 @Component({
@@ -12,17 +13,19 @@ export class StatusBarComponent implements OnInit {
   @Input() gameState: GameState;
   @Input() activePlayer: Player;
 
-  constructor() { }
+  constructor(private authService: AuthenticationService) { }
 
   ngOnInit(): void {
   }
 
   getMessage(): string {
+    const user = this.authService.getAuthenticatedUser();
+    
     switch(this.gameState) {
       case GameState.NOT_ENOUGH_PLAYERS: return `Waiting for more players`;
-      case GameState.IN_PROGRESS: return `Player ${this.activePlayer.name} is now moving`;
+      case GameState.IN_PROGRESS: return (user.name == this.activePlayer.name) ? "You are moving" : `Player ${this.activePlayer.name} is now moving`;
       case GameState.DRAW: return `Game tied`;
-      case GameState.WIN: return `Player ${this.activePlayer.name} WON!`;
+      case GameState.WIN: return (user.name == this.activePlayer.name) ? "You WON!" : `Player ${this.activePlayer.name} WON!`;
     }
   }
 
